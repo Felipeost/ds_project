027d3bfa551e08482970716f7d9647198858bdfc
Arina Godman <146754998+ArinaGodman@users.noreply.github.com>
2024-10-17 15:36:46 +0200

    Delete my.bat

--- my.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Activate the virtual environment
-CALL "C:\Users\lalka\OneDrive\Skrivbord\Studier\Projekt\working repo\ds_project\env\Scripts\activate.bat"
-
-REM Run the Python script
-python "C:\Users\lalka\OneDrive\Skrivbord\Studier\Projekt\working repo\ds_project\etl_polis_data.py"
-
-REM Pause to keep the command window open
-pause
